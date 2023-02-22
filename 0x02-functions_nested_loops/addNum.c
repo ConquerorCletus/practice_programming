5f293fd8a3d6949f0e09931486276696a93f6307
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-22 16:15:02 +0100

    addNum try

---
+++ 0x02-functions_nested_loops/addNum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Main -a program that adds 2 numbers
+ * Return:0
+ */
+int main(void)
+{
+	int a;
+
+	a = add(8, 9);
+	printf("%d \n", a);
+	return (0);
+}
