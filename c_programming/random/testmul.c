b3722179455d9fc7ebad17b3d8744356a28ac764
ConquerorCletus <smartchibuike2019@gmail.com>
2023-02-28 02:18:42 +0100

    testmul function

--- c_programming/random/testmul.c
+++ c_programming/random/testmul.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
  * main - This program test mul function
  * Return: 0
  */
 int main(void)
 {
-	int a = 8, b = 9;
-	mul(8, 9);
-	printf("%d\n", mul);
+	int testmul;
+
+	testmul = mul(8, 9);
+	printf("%d\n", testmul);
 	return (0);
 }
+/*
+ * This program test the "mul" function in main.h, it was written to explore the use of function definitions and how it works
+ */
