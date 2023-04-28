0e3369e5363dbb5c3493f91425a3faca3a183256
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-28 21:44:53 +0100

    convert pounds to kg

---
+++ Python_programming/convert.py
@@ -0,0 +1,4 @@
+weight = input("Input your weight (pounds):")
+mass = float(weight) / 2.2046
+print("Your weight is {}kg ".format(mass))
+# This program collect user weight as string as store it as a float value.
