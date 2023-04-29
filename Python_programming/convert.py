1f9ebd6e816ab6239108764951f31d1fd2cbbd3e
ConquerorCletus <smartchibuike2019@gmail.com>
2023-04-29 17:50:01 +0100

    Choice added to converter

--- Python_programming/convert.py
+++ Python_programming/convert.py
@@ -2,3 +2,17 @@ weight = input("Input your weight (pounds):")
 mass = float(weight) / 2.2046
 print("Your weight is {}kg ".format(mass))
 # This program collect user weight as string as store it as a float value.
+print("____________END OF PROGRAM____________________")
+print("_______________NEW PROGRAM_____________________")
+new_weight = input("Input your weight: ")
+new_weight = int(new_weight)
+choice = input("is your weight in (L)bs or (K)g: ")
+
+if choice.upper == "L":
+    new_weight = new_weight * 2.2046
+    print("Your weight is {} kg".format(new_weight))
+    
+else:
+    new_weight = new_weight / 2.2046
+    print("Your weight is {} pounds".format(new_weight))
+
