8c100a19ecceef25a88346cf494a716d9bb5d153
ConquerorCletus <smartchibuike2019@gmail.com>
2023-05-03 07:14:35 +0100

    PLD instance

---
+++ Python_programming/simple.py
@@ -0,0 +1,10 @@
+age = input("your age:")
+print("Myname = input("What is your name? ")
+ name is {}, i am {} year old ".format(name,age))
+birth_year = int(input("Your birth year: "))
+age = 2023 - birth_year
+print("you are {} year old.".format(age))
+
+
+array = [23,38,24,35]
+print(array)
