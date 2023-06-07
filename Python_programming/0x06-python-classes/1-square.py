66f61349edb302074332373bc1ec0841cf7836ff
ConquerorCletus <smartchibuike2019@gmail.com>
2023-06-07 16:28:44 +0000

    arranged for study

---
+++ Python_programming/0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" python Class"""
+
+
+class Square():
+    """square Class"""
+
+    def __init__(self, size):
+        """ initialize the class
+
+        Args:
+           size: size of the square
+        """
+        self.__size = size
