f02fff664cfb495255b1fccaa4f6459b52a9c36f
ConquerorCletus <smartchibuike2019@gmail.com>
2023-06-14 07:26:41 +0000

    oop tutorial

--- Python_programming/0x06-python-classes/0-square.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Square class that does nothing"""
-
-
-class Square:
-    """with pass i don't do anything, i just pass-by"""
-    pass
--- Python_programming/0x08-python-more_classes/0-rectangle.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-Defines an empty rectangle classes
-"""
-
-
-class Rectangle:
-    """This class does nothing"""
-    pass
